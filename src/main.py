f3f0ee5673903297bdcad0cddc7a76b1a7d888e9
abattalov <abattalov14@gmail.com>
2025-03-18 21:46:10 -0500

    added ability to create textnodes

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-print("hello world")
+from textnode import TextType, TextNode
+
+def main():
+    node = TextNode("hello world", TextType.TEXT)
+    print(node)
+
+if __name__ == "__main__":
+    main()
